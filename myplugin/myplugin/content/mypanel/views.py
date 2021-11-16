e25edd029b76e563839e15e7438281d17a5e24a9
Shamika Joshi <shamika.jsh@gmail.com>
2021-11-16 11:11:09 -0800

    AutoVM: Updating data to render from view.py

--- myplugin/myplugin/content/mypanel/views.py
+++ myplugin/myplugin/content/mypanel/views.py
@@ -5,6 +5,19 @@ class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
     
     def get_context_data(self, *args, **kwargs):
-        data = {'message': 'Hello Django!'}
+        data = {
+            'network incoming bytes': 10646, 
+            'network outgoing bytes': 10586, 
+            'disk device write bytes': 46475264, 
+            'network outgoing packets': 116, 
+            'memory usage': 14.609375, 
+            'disk device read requests': 1041, 
+            'network incoming packets': 101, 
+            'cpu': 175930000000, 
+            'disk device write requests': 705, 
+            'disk device read bytes': 21753344, 
+            'image download': 12716032, 
+            'image serve': 12716032
+            }
         context = data
         return context
