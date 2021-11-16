4795d30f4561311b423597e1705b41830e4e488a
Shamika Joshi <shamika.jsh@gmail.com>
2021-11-16 13:07:26 -0800

    AutoVM: Testing view.py

--- myplugin/myplugin/content/mypanel/views.py
+++ myplugin/myplugin/content/mypanel/views.py
@@ -6,12 +6,18 @@ class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
     
     def get_context_data(self, *args, **kwargs):
-        data = {}
-        with open('ceiltest') as f:
-            lines = f.readlines()
-            for line in reversed(lines):
-                key = (json.loads(line)['name'])
-                if key not in data.keys():
-                    data[key] = json.loads(line)['volume']
+        data = {
+            'disk.device.write.requests': 783, 
+            'network.outgoing.bytes': 15698, 
+            'network.incoming.packets': 283, 
+            'disk.device.write.bytes': 46624768, 
+            'disk.device.read.requests': 1042, 
+            'memory.usage': 14.546875, 
+            'disk.device.read.bytes': 21754368, 
+            'network.incoming.bytes': 26542, 
+            'cpu': 1665930000000, 
+            'network.outgoing.packets': 152
+        }
+        
         context = data
         return context
