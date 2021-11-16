3a27a34192f5f30cefbb9ed5a01afa452f895876
Shamika Joshi <shamika.jsh@gmail.com>
2021-11-16 12:40:58 -0800

    AutoVM: Make code generic

--- myplugin/myplugin/content/mypanel/views.py
+++ myplugin/myplugin/content/mypanel/views.py
@@ -1,23 +1,17 @@
 from django.views import generic
+import json
 
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
     
     def get_context_data(self, *args, **kwargs):
-        data = {
-            'network incoming bytes': 10646, 
-            'network outgoing bytes': 10586, 
-            'disk device write bytes': 46475264, 
-            'network outgoing packets': 116, 
-            'memory usage': 14.609375, 
-            'disk device read requests': 1041, 
-            'network incoming packets': 101, 
-            'cpu': 175930000000, 
-            'disk device write requests': 705, 
-            'disk device read bytes': 21753344, 
-            'image download': 12716032, 
-            'image serve': 12716032
-            }
+        data = {}
+        with open('ceiltest') as f:
+            lines = f.readlines()
+            for line in reversed(lines):
+                key = (json.loads(line)['name'])
+                if key not in data.keys():
+                    data[key] = json.loads(line)['volume']
         context = data
         return context
