cba30d048cbd9741aafa419e3374c234c8dd6be3
Shamika Joshi <shamika.jsh@gmail.com>
2021-11-16 08:23:45 -0800

    AutoVM: testing view.py context

--- myplugin/myplugin/content/mypanel/views.py
+++ myplugin/myplugin/content/mypanel/views.py
@@ -3,3 +3,8 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(IndexView. self).get_context_data(*args, **kwargs)
+        context['message'] = 'Hello World!'
+        return context
